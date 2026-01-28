520b9663ba4971de8fc4a405518bd1a7ba8e4736
Antti Kervinen <antti.kervinen@intel.com>
2026-01-28 19:44:32 +0200

    cpu optimization: improve tp4 and add tp8 generation
    - Reduce number of generated tp4 combinations.
    - Generate small number of tp8 combinations.
    - Tested up to 8-socket SNC4 systems.
    
    Signed-off-by: Antti Kervinen <antti.kervinen@intel.com>

--- core/scripts/gen-balloon-types.py
+++ core/scripts/gen-balloon-types.py
@@ -1,31 +1,33 @@
 #!/usr/bin/env python3
 
-"""Generate balloon types for vLLM tensor parallelism sizes 1, 2 and 4.
+"""Generate balloon types for vLLM tensor parallelism sizes 1, 2, 4 and 8.
 
 Usage: gen-balloon-types.py > balloon-types.yaml
 
 This scripts reads system hardware topology (CPU packages and their
 NUMA nodes) and generates balloon types for vLLM containers that need
-an equal number of exclusive CPUs from 1, 2 or 4 NUMA nodes. The
+an equal number of exclusive CPUs from 1, 2, 4 or 8 NUMA nodes. The
 output can be included in the "balloonTypes:" list of NRI balloons
 resource policy configuration. Containers are associated with correct
 balloon type with one of the following pod annotations:
 
 balloon.balloons.resource-policy.nri.io: vllm-balloon-tp1
 balloon.balloons.resource-policy.nri.io: vllm-balloon-tp2
-balloon.balloons.resource-policy.nri.io: vllm-balloon-tp3
+balloon.balloons.resource-policy.nri.io: vllm-balloon-tp4
+balloon.balloons.resource-policy.nri.io: vllm-balloon-tp8
 
 Environment variables:
 
   PKG_NODE      optional, Python dictionary that associates physical
-                CPU package IDs to lists of NUMA node IDs on each package.
+                CPU package IDs to lists of NUMA node IDs on each package:
+                {pkg0id: [pkg0node0id,...],...}
                 If not set, the script builds the dictionary from
                 /sys/devices/system/node/node*/cpu*/topology/physical_package_id
 
   MAX_TP        optional, maximum tensor parallelism level to generate balloon
-                types for. The default is 4. Note that tp2 and tp4 balloons will
-                not be generated unless there are at least 2 or 4 NUMA nodes in
-                the system (or PKG_NODE corresponding to a system).
+                types for. The default is 4. Note that tp2, tp4 and tp8 balloons
+                will not be generated unless there are at least 2, 4 or 8 NUMA
+                nodes in the system (or PKG_NODE corresponding to a system).
 
   BALLOON_NAME  optional, base name for balloon types. The default is "vllm-balloon".
 
@@ -59,6 +61,26 @@ balloon_type_common = """
 def error(msg):
     sys.stderr.write(f"gen-balloon-types.py error: {msg}\n")
     sys.exit(1)
+
+# rotcov_combinations yields combinations of n elements until
+# all elements are covered in at least one combination and
+# equally many times in all yielded combinations.
+def rotcov_combinations(elts, n):
+    if n > len(elts):
+        return
+    idx_count = {i: 0 for i in range(len(elts))}
+    next_comb_start_idx = 0
+    comb = [None] * n
+    while True:
+        for i in range(n):
+            idx = (next_comb_start_idx + i) % len(elts)
+            comb[i] = elts[idx]
+            idx_count[idx] += 1
+        yield tuple(comb)
+        next_comb_start_idx = (next_comb_start_idx + n) % len(elts)
+        ref_count = idx_count[0]
+        if all(idx_count[i] == ref_count for i in idx_count):
+            break
 
 # optimize_gnr3tile iterates nodes in the order of preference for
 # using those nodes, assuming that CPUs will be selected only from
@@ -104,6 +126,9 @@ def generate_balloon_types(pkg_node, max_tp):
     tp4_balloon_types = []
     tp4_balloon_names = []
 
+    tp8_balloon_types = []
+    tp8_balloon_names = []
+
     # Add base balloon types for allocating CPUs on each NUMA node.
     # "|" in the beginning of each *_balloon_type line indicates the
     # indention level of "balloonTypes" in balloons policy yaml.
@@ -132,32 +157,35 @@ def generate_balloon_types(pkg_node, max_tp):
 
     # Generate -tp2 balloon types only if there are at least 2 nodes in the system.
     if max_tp >= 2 and len(node_pkg) >= 2:
+        locality = "localpkg"
         two_node_sets = []
         # Prefer creating -tp2 balloons from two nodes local to the same
         # package.
-        if all(len(pkg_node[pkg]) >= 2 for pkg in pkg_node):
+        if all(len(pkg_node[pkg]) >= 2 and len(pkg_node[pkg]) % 2 == 0 for pkg in pkg_node):
+            # As number of nodes in each package is divisible by two,
+            # create non-overlapping sets of two nodes. Lowers the
+            # number of node combinations.
+            for pkg in pkg_node:
+                selected = set()
+                for comb in itertools.combinations(pkg_node[pkg], 2):
+                    if all(node not in selected for node in comb):
+                        two_node_sets.append(comb)
+                        selected.add(comb[0])
+                        selected.add(comb[1])
+        elif all(len(pkg_node[pkg]) >= 2 for pkg in pkg_node):
             for pkg in pkg_node:
                 two_node_sets.extend(comb for comb in itertools.combinations(pkg_node[pkg], 2))
+        else:
+            # Cannot create -tp2 balloons using always nodes local to a package.
+            # Create combinations that span two packages.
+            two_node_sets = [comb for comb in itertools.combinations(node_pkg, 2)
+                             if node_pkg[comb[0]] != node_pkg[comb[1]]]
+            locality = "crosspkg"
 
         if len(two_node_sets) > 0:
             for i, nodeset in enumerate(two_node_sets):
                 pkg0, pkg1 = node_pkg[nodeset[0]], node_pkg[nodeset[1]]
-                tp2_balloon_names.append(f"localpkgcomb{i}-tp2")
-                tp2_balloon_types.append(f"""
-                |- name: {tp2_balloon_names[-1]}
-                |  componentCreation: all
-                |  components:
-                |  - balloonType: pkg{pkg0}node{nodeset[0]}
-                |  - balloonType: pkg{pkg1}node{nodeset[1]}
-                """)
-        else:
-            # Cannot create -tp2 balloons using always nodes local to a package.
-            # Create combinations that span two packages.
-            two_remote_node_sets = [comb for comb in itertools.combinations(node_pkg, 2)
-                                    if node_pkg[comb[0]] != node_pkg[comb[1]]]
-            for i, nodeset in enumerate(two_remote_node_sets):
-                pkg0, pkg1 = node_pkg[nodeset[0]], node_pkg[nodeset[1]]
-                tp2_balloon_names.append(f"crosspkgcomb{i}-tp2")
+                tp2_balloon_names.append(f"{locality}comb{i}-tp2")
                 tp2_balloon_types.append(f"""
                 |- name: {tp2_balloon_names[-1]}
                 |  componentCreation: all
@@ -180,15 +208,51 @@ def generate_balloon_types(pkg_node, max_tp):
 
     # Generate -tp4 balloon types only if there are at least 4 nodes in the system.
     if max_tp >= 4 and len(node_pkg) >= 4:
-        four_node_sets = None
-        if len(pkg_node) == 2 and all(len(pkg_node[pkg]) >= 2 for pkg in pkg_node):
-            # In a two-socket system, create cross-package -tp4 balloons where
-            # each package contributes two nodes.
+        locality = "crosspkg"
+        four_node_sets = []
+        if all(len(pkg_node[pkg]) >= 4 and len(pkg_node[pkg]) % 4 == 0 for pkg in pkg_node):
+            # As the number of nodes in each package is divisible by four,
+            # create non-overlapping sets of four nodes. Significantly lowers the
+            # number of node combinations.
+            locality = "localpkg"
+            for pkg in pkg_node:
+                selected = set()
+                for comb in itertools.combinations(pkg_node[pkg], 4):
+                    if all(node not in selected for node in comb):
+                        four_node_sets.append(comb)
+                        selected.update(comb)
+
+        elif all(len(pkg_node[pkg]) >= 4 and len(pkg_node[pkg]) % 2 == 0 for pkg in pkg_node):
+            # As the number of nodes in each package is divisible by
+            # two, create non-overlapping sets of two nodes and create
+            # four-node sets by combining those. Lowers the number of
+            # node combinations.
+            locality = "localpkg"
+            for pkg in pkg_node:
+                selected = set()
+                non_overlapping_two_nodes = []
+                for comb in itertools.combinations(pkg_node[pkg], 2):
+                    if all(node not in selected for node in comb):
+                        non_overlapping_two_nodes.append(comb)
+                        selected.update(comb)
+                for tns in itertools.combinations(non_overlapping_two_nodes, 2):
+                    four_node_sets.append(tns[0] + tns[1])
+
+        elif all(len(pkg_node[pkg]) >= 4 for pkg in pkg_node):
+            # If every package has at least four nodes, create only localpkg -tp4 balloons.
+            locality = "localpkg"
+            for pkg in pkg_node:
+                four_node_sets.extend(comb for comb in itertools.combinations(pkg_node[pkg], 4))
+        elif len(pkg_node) == 2 and all(len(pkg_node[pkg]) >= 2 for pkg in pkg_node):
+            # In a two-socket system with at least two nodes in each
+            # package, create cross-package -tp4 balloons where each
+            # package contributes two nodes.
             pkgs = sorted(pkg_node)
             pkg0, pkg1 = pkgs[0], pkgs[1]
             pkg0_nodesets = list(itertools.combinations(pkg_node[pkg0], 2))
             pkg1_nodesets = list(itertools.combinations(pkg_node[pkg1], 2))
             four_node_sets = [p0 + p1 for p0, p1 in zip(pkg0_nodesets, pkg1_nodesets)]
+
         elif len(pkg_node) == 4:
             if all(len(pkg_node[pkg]) == 1 for pkg in pkg_node):
                 # 4-socket system, one node per package, create -tp4 balloons
@@ -203,13 +267,12 @@ def generate_balloon_types(pkg_node, max_tp):
                 # pkgs01_nodeset = pkg_node[pkg0] + pkg_node[pkg1]
                 pkgs23_nodesets = [p2ns + p3ns for p2ns, p3ns in zip(itertools.combinations(pkg_node[pkg2], 2), itertools.combinations(pkg_node[pkg3], 2))]
                 four_node_sets = pkgs01_nodesets + pkgs23_nodesets
-        if not four_node_sets:
-            # A single socket system, 5+ socket system, or a system
-            # with mixed number of nodes per package so that we cannot
-            # pick two nodes from each package. Keep the number of
-            # cross-package -tp4 balloon types manageable by dividing
-            # nodes into quadrants and selecting one node from each
-            # quadrant.
+        else:
+            # 5+ socket system, or a system with mixed number of nodes
+            # per package so that we cannot pick two nodes from each
+            # package. Keep the number of cross-package -tp4 balloon
+            # types manageable by dividing nodes into quadrants and
+            # selecting one node from each quadrant.
             node_quadrant = {}
             for node_index, node in enumerate(sorted(node_pkg)):
                 node_quadrant[node] = node_index // 4
@@ -218,7 +281,7 @@ def generate_balloon_types(pkg_node, max_tp):
                                   node_quadrant[comb[2]] < node_quadrant[comb[3]])]
         for i, nodeset in enumerate(four_node_sets):
             pkg0, pkg1, pkg2, pkg3 = node_pkg[nodeset[0]], node_pkg[nodeset[1]], node_pkg[nodeset[2]], node_pkg[nodeset[3]]
-            tp4_balloon_names.append(f"crosspkgcomb{i}-tp4")
+            tp4_balloon_names.append(f"{locality}comb{i}-tp4")
             tp4_balloon_types.append(f"""
             |- name: {tp4_balloon_names[-1]}
             |  componentCreation: all
@@ -239,9 +302,48 @@ def generate_balloon_types(pkg_node, max_tp):
             tp4_balloon_types.append(f"""
             |  - balloonType: {name}
             """)
+    # Generate -tp8 balloon types only if there are at least 8 nodes in the system.
+    if max_tp >= 8 and len(node_pkg) >= 8:
+        locality = "crosspkg"
+        eight_node_sets = []
+        eight_node_comb_limit = 16
+        for comb in rotcov_combinations(sorted(node_pkg), 8):
+            eight_node_sets.append(comb)
+            if len(eight_node_sets) >= eight_node_comb_limit:
+                break
+        for i, nodeset in enumerate(eight_node_sets):
+            pkg0, pkg1, pkg2, pkg3, pkg4, pkg5, pkg6, pkg7 = (
+                node_pkg[nodeset[0]], node_pkg[nodeset[1]], node_pkg[nodeset[2]], node_pkg[nodeset[3]],
+                node_pkg[nodeset[4]], node_pkg[nodeset[5]], node_pkg[nodeset[6]], node_pkg[nodeset[7]])
+            tp8_balloon_names.append(f"{locality}comb{i}-tp8")
+            tp8_balloon_types.append(f"""
+            |- name: {tp8_balloon_names[-1]}
+            |  componentCreation: all
+            |  components:
+            |  - balloonType: pkg{pkg0}node{nodeset[0]}
+            |  - balloonType: pkg{pkg1}node{nodeset[1]}
+            |  - balloonType: pkg{pkg2}node{nodeset[2]}
+            |  - balloonType: pkg{pkg3}node{nodeset[3]}
+            |  - balloonType: pkg{pkg4}node{nodeset[4]}
+            |  - balloonType: pkg{pkg5}node{nodeset[5]}
+            |  - balloonType: pkg{pkg6}node{nodeset[6]}
+            |  - balloonType: pkg{pkg7}node{nodeset[7]}
+            """)
+
+        tp8_balloon_types.append(f"""
+        |- name: {balloon_name}-tp8
+        {balloon_type_common}
+        |  componentCreation: balance-balloons
+        |  components:
+        """)
+        for name in tp8_balloon_names:
+            tp8_balloon_types.append(f"""
+            |  - balloonType: {name}
+            """)
+
 
     balloon_types = []
-    for lines in node_balloon_types + tp1_balloon_types + tp2_balloon_types + tp4_balloon_types:
+    for lines in node_balloon_types + tp1_balloon_types + tp2_balloon_types + tp4_balloon_types + tp8_balloon_types:
         for line in lines.splitlines():
             if line.strip().startswith("|"):
                 balloon_types.append(line.strip()[1:])
@@ -258,7 +360,7 @@ if __name__ == "__main__":
     try:
         max_tp = int(os.getenv("MAX_TP", "4"))
     except Exception as e:
-        error(f"failed to parse MAX_TP value: {e}, expected 1, 2 or 4")
+        error(f"failed to parse MAX_TP value: {e}, expected 1, 2, 4 or 8")
 
     try:
         indent = int(os.getenv("INDENT", "0"))
